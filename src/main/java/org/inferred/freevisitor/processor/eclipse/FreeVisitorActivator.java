39c5a9a832ea519ce967bbdc13e2f61c6c21cb17
Chris Purcell <Chris.Purcell.39@gmail.com>
2016-06-29 19:50:24 +0100

    Get Eclipse working 'properly', after much struggle

--- src/main/java/org/inferred/freevisitor/processor/eclipse/FreeVisitorActivator.java
+++ src/main/java/org/inferred/freevisitor/processor/eclipse/FreeVisitorActivator.java
@@ -3,28 +3,21 @@ package org.inferred.freevisitor.processor.eclipse;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
 public class FreeVisitorActivator extends Plugin {
 
   // The plug-in ID
-  public static final String PLUGIN_ID = "sk.seges.corpis.core.pap.transfer";
+  public static final String PLUGIN_ID = "org.inferred.freevisitor";
 
   // The shared instance
   private static FreeVisitorActivator plugin;
 
-  /**
-   * The constructor
-   */
-  public FreeVisitorActivator() {
-  }
-
+  @Override
   public void start(BundleContext context) throws Exception {
     super.start(context);
     plugin = this;
   }
 
+  @Override
   public void stop(BundleContext context) throws Exception {
     plugin = null;
     super.stop(context);
